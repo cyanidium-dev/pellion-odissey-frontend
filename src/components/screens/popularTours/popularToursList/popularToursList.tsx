7a7351b627c1e3ee8ec0825423fcd9d0cd68a13a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-02 14:32:27 +0300

    connected sanity data to the home page

--- src/components/screens/popularTours/popularToursList/popularToursList.tsx
+++ src/components/screens/popularTours/popularToursList/popularToursList.tsx
@@ -1,42 +1,41 @@
 "use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import style from "./popularToursList.module.scss";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css/skyblue";
 import { TitleAndText } from "@/components/shared/titleAndText/titleAndText";
-import Image from 'next/image';
-import { LittleCard, LittleCardStyles } from '@/components/shared/LittleCard';
-
-const data = [
-  {
-    img: "https://optim.tildacdn.com/stor3962-6137-4430-a134-323039383331/-/resize/432x/-/format/webp/87684469.jpg",
-    hashtags: "сша - калифорния - экскурсии - природа - джипы - 2025",
-    title: "США: национальные парки Калифорнии",
-    date: "25 октября - 3 ноября 2025",
-    details: "10 дней - группа до 12 человек",
-    price: "4 000$",
-  },
-  {
-    img: "https://optim.tildacdn.com/stor3736-3962-4562-b235-353532623161/-/resize/432x/-/format/webp/50641660.png",
-    hashtags: "перу - боливия - чили - экскурсии - хайкинг - 2025",
-    title: "ПЕРУ-БОЛИВИЯ-ЧИЛИ: большое путешествие ",
-    date: "18 - 31 октября  2025 (предзапись)",
-    details: "14 дней - группа до 13 человек",
-    price: "4 950$",
-  },
-  {
-    img: "https://optim.tildacdn.com/stor3637-6638-4130-b131-313233633930/-/resize/432x/-/format/webp/91421140.jpg",
-    hashtags: "перу - боливия - чили - экскурсии - хайкинг - 2025",
-    title: "ПЕРУ-БОЛИВИЯ-ЧИЛИ: большое путешествие ",
-    date: "8 - 21 ноября 2025",
-    details: "14 дней - группа до 13 человек",
-    price: "4 950$",
-  },
-];
+import Image from "next/image";
+import { LittleCard, LittleCardStyles } from "@/components/shared/LittleCard";
+import { fetchSanityData } from "@/utils/fetchSanityData";
+import { allPopularToursQuery } from "@/lib/queries";
 
 export const PopularToursList: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [tours, setTours] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // Get all tours from cms
+  useEffect(() => {
+    const getTours = async () => {
+      try {
+        const data = await fetchSanityData(allPopularToursQuery);
+        console.log(data);
+        setTours(data);
+      } catch (error) {
+        console.error("Error fetching tours:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getTours();
+  }, []);
+
+  if (!tours.length) return null;
+
   return (
-    <section className={style.popularToursList} >
+    <section className={style.popularToursList}>
       <TitleAndText
         title="Самые популярные авторские туры"
         text="Присоединяйся"
@@ -44,48 +43,57 @@ export const PopularToursList: React.FC = () => {
         textClassName={style.text}
         className={style.titleAndText}
       />
-      
-      <Image src={"/images/other/grape.png"} alt={"flag"} className={style.grape} width={300} height={400}/>
 
-      {data.length > 3 ? (
-       
-          <Splide
-            className={style.slider}
-            options={{
-              perPage: 3,
-              interval: 3000, 
-              speed: 1000, 
-              pauseOnHover: false,
-              pauseOnFocus: false,
-              breakpoints: {
-                1290: {
-                  perPage: 1,
-                  drag: true,
-                },
-                500: {
-                  arrows: false,
-                }
+      <Image
+        src={"/images/other/grape.png"}
+        alt={"flag"}
+        className={style.grape}
+        width={300}
+        height={400}
+      />
+
+      {tours.length > 3 ? (
+        <Splide
+          className={style.slider}
+          options={{
+            perPage: 3,
+            interval: 3000,
+            speed: 1000,
+            pauseOnHover: false,
+            pauseOnFocus: false,
+            breakpoints: {
+              1290: {
+                perPage: 1,
+                drag: true,
               },
-              type: "loop",
-              gap: "30px",
-              drag: false,
-            }}
-          >
-            {data.map((item, index) => (
-              <SplideSlide key={index}>
-                <LittleCard item={item} />
-              </SplideSlide>
-            ))}
-          </Splide>
-        
+              500: {
+                arrows: false,
+              },
+            },
+            type: "loop",
+            gap: "30px",
+            drag: false,
+          }}
+        >
+          {tours.map((item, index) => (
+            <SplideSlide key={index}>
+              <LittleCard item={item} />
+            </SplideSlide>
+          ))}
+        </Splide>
       ) : (
         <div className={style.items}>
-          {data.map((item, index) => (
+          {tours.map((item, index) => (
             <LittleCard key={index} item={item} />
           ))}
         </div>
       )}
-      <Link href={"/tours"} className={`${LittleCardStyles.detailsButton} ${style.button}`}>Смотреть все</Link>
+      <Link
+        href={"/tours"}
+        className={`${LittleCardStyles.detailsButton} ${style.button}`}
+      >
+        Смотреть все
+      </Link>
     </section>
   );
 };
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -17,7 +17,32 @@ export const allToursQuery = `
     "tourType": tourType,
     "season": season,
     "years": years,
-    "hashtags": hashtags
+    "hashtags": hashtags,
+    "isPopular": isPopular
+  }
+`;
+
+export const allPopularToursQuery = `
+  *[_type == "tour" && isPopular == true] | order(_createdAt desc) {
+    "id": _id,
+    "slug": slug.current,
+    "title": title,
+    "price": price,
+    "duration": {
+      "days": duration.days,
+      "nights": duration.nights
+    },
+    "group": group,
+    "gallery": gallery[].asset->url,
+    "tourDates": tourDates[] {
+      startDate,
+      endDate
+    },
+    "tourType": tourType,
+    "season": season,
+    "years": years,
+    "hashtags": hashtags,
+    "isPopular": isPopular
   }
 `;
 
